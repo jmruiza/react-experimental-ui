199b3d188a3236eb1b2c747ee6da1fa8ab4a4f48
JM Ruiz <jmanuel.rar@gmail.com>
2020-03-12 14:14:54 -0600

    Add file strcuture

---
+++ src/data/dummy.js
@@ -0,0 +1,7 @@
+import faker from 'faker';
+
+export default () => {
+    // TODO: Create report data dynamically
+    // TODO: Create article data dynamically
+    // TODO: Create section data dynamically
+}
