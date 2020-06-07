29e97b99abb43339c5845a22c0a14f41f07647dd
JM Ruiz <jmanuel.rar@gmail.com>
2020-06-07 09:13:46 -0500

    Define context

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const MeetupContext = React.createContext();
+
+const initialState = {
+  title: 'Auth0 Online Meetup',
+  date: Date()
+};
+
+const App = () => (
+  <MeetupContext.Provider value={initialState}>
+    <MeetupContext.Consumer>
+      {props => (
+        <div>
+          <h1>{props.title}</h1>
+          <span>{props.date}</span>
+        </div>
+      )}
+    </MeetupContext.Consumer>
+  </MeetupContext.Provider>
+);
 
 export default App;
